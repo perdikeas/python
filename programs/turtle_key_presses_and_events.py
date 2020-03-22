35da5ae4ebc77bd70346cb135b0cd79c88f63434
Constantine Perdikeas <cperdikeas@gmail.com>
2020-03-22 23:49:49 +0200

    bug fixes

--- programs/turtle_key_presses_and_events.py
+++ programs/turtle_key_presses_and_events.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
 import turtle
 import random
+import time
 
 colors=['red','black','gold','blue']
 
 tim=turtle.Turtle()
 tim.speed(5)
-tim.width(5)
+tim.width(3)
 tim.shape('arrow')
 tim.setpos(0,0)
 
-len=100
+len=30
 def up():
     global len
     tim.setheading(90)
@@ -31,24 +32,40 @@ def left():
     tim.setheading(180)
     tim.forward(len)
 
-def click_left_mouse_button(x,y):
+def dragging(x,y):
+    global colors
+    turtle.ondrag(None)
     color1=random.choice(colors)
     color2=random.choice(colors)
     tim.color(color1,color2)
-    tim.forward(5)
-
+    tim.setheading(tim.towards(x,y))
+    tim.goto(x,y)
+    turtle.ondrag(dragging)
+def click_left_mouse_button(x,y):
+    global colors
+    color1=random.choice(colors)
+    color2=random.choice(colors)
+    tim.color(color1,color2)
+    tim.forward(20)
 def click_right_mouse_button(x,y):
     tim.stamp()
 
 def click_x_key():
     turtle.clearscreen()
 
+turtle.color('black')
+tim.hideturtle()
+style=('Courier',10,'bold')
+turtle.write("press arrow keys,right and left click and drag the turtle from its starting position",font=style,align='center')
+time.sleep(6)
+tim.showturtle()
+turtle.clearscreen()
+
 turtle.onscreenclick(click_left_mouse_button,1)
 turtle.onscreenclick(click_right_mouse_button,3)
-turtle.ondrag(click_left_mouse_button,1)
-turtle.ondrag(click_right_mouse_button,3)
-turtle.onrelease(click_left_mouse_button,1)
-turtle.onrelease(click_right_mouse_button,3)
+turtle.ondrag(dragging)
+
+
 
 turtle.onkey(click_x_key,'x')
 turtle.onkey(up,'Up')
