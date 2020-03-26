f4b14c16b6a8f5a1f0d7d368f00e353189a7664c
Constantine Perdikeas <cperdikeas@gmail.com>
2020-03-26 22:42:40 +0200

    health bars and killing goblins

--- programs/pygames/pygame2.py
+++ programs/pygames/pygame2.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python3
 
+
+
 import random
 import pygame
+import math
+import time
+
+
 
 pygame.init()
 
+
+
 #global variables
+
+
 game_running=True
 Screenwidth=500
 Screenheight=480
@@ -13,6 +23,24 @@ bullets=[]
 counter=0
 enemies=[]
 
+
+
+#rgb values
+
+
+#darkgreen
+color1=(0,128,0)
+
+#orange
+color2=(255,165,0)
+
+#darkred
+color3=(128,0,0)
+
+
+
+#font for writing
+font=pygame.font.SysFont('comicsans',30,True,True)
 
 #enemy sprite images
 
@@ -49,6 +77,59 @@ char = pygame.image.load('./images/standing.png')
 clock=pygame.time.Clock()
 
 #classes and functions
+def sqrt(a):
+    return math.sqrt(a)
+
+def distance_between(a,b):
+    return math.sqrt(math.pow(a.y-b.y,2)+math.pow(a.x-b.x,2))
+
+
+
+
+
+def player_health_checking(player):
+    global enemies,game_running,win,font
+
+    necessary_kills=1
+
+
+    if player.kills<necessary_kills:
+        if player.health<=player.starting_health and player.health>=player.starting_health-5:
+            pygame.draw.rect(win,color1,(player.x,player.y-20,55+player.health-player.starting_health,10))
+        elif player.health<player.starting_health-5 and player.health>=player.starting_health-15:
+            pygame.draw.rect(win,color2,(player.x,player.y-20,55+player.health-player.starting_health,10))
+        elif player.health<player.starting_health-15 and player.health>0:
+            pygame.draw.rect(win,color3,(player.x,player.y-20,55+player.health-player.starting_health,10))
+        else:
+            player.visible=False
+            game_running=False
+            for enemy in enemies:
+                enemy.visible=False
+            win.fill((255,255,255))
+            text3=font.render('The goblins killed you',1,(0,0,0))
+            win.blit(text3,(150,250))
+            pygame.display.update()
+            time.sleep(4)
+            pygame.quit()
+    else:
+        player.visible=False
+        game_running=False
+
+        for enemy in enemies:
+            enemy.visible=False
+
+        win.fill((255,255,255))
+
+        text4=font.render('You slayed 10 goblins',1,(0,0,0))
+
+        win.blit(text4,(150,250))
+
+        pygame.display.update()
+        time.sleep(4)
+        pygame.quit()
+
+
+
 
 #enemy class
 class Enemy():
@@ -63,9 +144,23 @@ class Enemy():
         self.height=height
         self.walk_count=0
         self.vel=3
-
-
+        self.health=20
+        self.starting_health=20
+        self.visible=True
     def live(self,win):
+        global enemies,man
+
+        if self.health<=20 and self.health>15:
+            pygame.draw.rect(win,color1,(self.x,self.y-20,55+self.health-self.starting_health,10))
+        elif self.health<=15 and self.health>5:
+            pygame.draw.rect(win,color2,(self.x,self.y-20,55+self.health-self.starting_health,10))
+        elif self.health<=5 and self.health>0:
+            pygame.draw.rect(win,color3,(self.x,self.y-20,55+self.health-self.starting_health,10))
+        else:
+            man.kills+=1
+            self.visible=False
+            enemies.remove(self)
+
 
         if self.walk_count+1 >= 33:
             self.walk_count=0
@@ -100,8 +195,6 @@ class Enemy():
 
 
 
-
-
 #player class
 class Player():
     def __init__(self,x,y,width,height):
@@ -109,35 +202,44 @@ class Player():
         self.y=y
         self.width=width
         self.height=height
-        self.velocity=5
+        self.velocity=4
         self.is_jump=False
         self.jump_count=10
         self.left=False
         self.right=False
         self.walk_count=0
         self.standing=True
-    def draw(self):
-        if  not(self.standing):
-            if self.left:
-                win.blit(walkLeft[self.walk_count//3],(self.x,self.y))
-                self.walk_count+=1
-            elif self.right:
-                win.blit(walkRight[self.walk_count//3],(self.x,self.y))
-                self.walk_count+=1
-        else:
-            if self.right:
-                win.blit(walkRight[0],(self.x,self.y))
-                self.walk_count+=1
-            else:
-                win.blit(walkLeft[0],(self.x,self.y))
-                self.walk_count+=1
+        self.health=20
+        self.starting_health=20
+        self.visible=True
+        self.kills=0
 
-        if self.walk_count+1>=27:
-            self.walk_count=0
+
+    def draw(self):
+        if self.visible:
+            if not(self.standing):
+                if self.left:
+                    win.blit(walkLeft[self.walk_count//3],(self.x,self.y))
+                    self.walk_count+=1
+                elif self.right:
+                    win.blit(walkRight[self.walk_count//3],(self.x,self.y))
+                    self.walk_count+=1
+            else:
+                if self.right:
+                    win.blit(walkRight[0],(self.x,self.y))
+                    self.walk_count+=1
+                else:
+                    win.blit(walkLeft[0],(self.x,self.y))
+                    self.walk_count+=1
+
+            if self.walk_count+1>=27:
+                self.walk_count=0
 
         pygame.display.update()
 
+
 #creating projectile/bullet class
+
 class Projectile():
     def __init__(self,x,y,radius,color,facing):
         self.x=x
@@ -146,6 +248,7 @@ class Projectile():
         self.color=color
         self.facing=facing
         self.vel=15*facing
+
     def draw(self,win):
         pygame.draw.circle(win,self.color,(self.x,self.y),self.radius)
 
@@ -156,36 +259,59 @@ man=Player(50,400,64,64)
 
 
 def redraw_window():
+
     win.blit(bg,(0,0))
+
     man.draw()
+    text1=font.render('Man health: {}'.format(man.health),1,(0,0,0))
+    text2=font.render('Man now has {} kills'.format(man.kills),1,(0,0,0))
+
+    win.blit(text1,(330,50))
+    win.blit(text2,(40,50))
+
     for bullet in bullets:
         bullet.draw(win)
+
     pygame.display.update()
+
+
 
 
 #we begin by adding only one opponent
 enemies.append(Enemy(400,400,64,64))
 
+
+
 while game_running:
+
+
     #30 fps
     clock.tick(30)
 
+
+    #drawing functions
     redraw_window()
 
-
+    player_health_checking(man)
+    #adding new enemies if there are none on the screen
     counter+=1
 
-    if (counter%200==0) and len(enemies)<2:
-        enemies.append(Enemy(random.randint(50,350),400,64,64))
+    #always have enemies on the screen
+    if  len(enemies)<1:
+        enemies.append(Enemy(random.randint(50,400),400,64,64))
 
 
     #enemy movement
     for enemy in enemies:
         enemy.live(win)
+        if distance_between(enemy,man)<10:
+            man.health-=0.5
 
     for event in pygame.event.get():
+
         if event.type==pygame.QUIT:
             game_running=False
+
 
     #bullet movement
     for bullet in bullets:
@@ -193,6 +319,13 @@ while game_running:
             bullet.x+=bullet.vel
         else:
             bullets.remove(bullet)
+        for enemy in enemies:
+            if distance_between(bullet,enemy)<enemy.width:
+                bullets.remove(bullet)
+                enemy.health-=1
+
+
+
 
     #list of pressed keys
     keys=pygame.key.get_pressed()
@@ -204,8 +337,8 @@ while game_running:
             facing=-1
         else:
             facing=1
-        if len(bullets)<5:
-            bullets.append(Projectile(round(man.x+man.width//2),round(man.y+man.height//2),6,(255,0,0),facing))
+        if len(bullets)<3:
+            bullets.append(Projectile(round(man.x+man.width//2),round(man.y+man.height//2),6,(0,0,0),facing))
 
 
 
@@ -255,4 +388,3 @@ while game_running:
 
 
     win.blit(bg,(0,0))
-pygame.quit()
