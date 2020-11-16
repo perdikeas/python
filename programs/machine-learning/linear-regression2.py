6741a52e907908008b0e2fd183a95be581bfe203
constantine <cperdikeas@gmail.com>
2020-11-16 22:39:38 +0200

    linear regression

--- programs/machine-learning/linear-regression2.py
+++ programs/machine-learning/linear-regression2.py
@@ -1,14 +1,1 @@
-#!/usr/bin/env python
-import pandas as pd
-import numpy as np
-import sklearn
-from sklearn import linear_model
-from sklearn.utils import shuffle
-import matplotlib.pyplot as pyplot
-import pickle
-from matplotlib import style
-
-data=pd.read_csv("student-mat.csv",sep=';')
-
-data=data[["G1","G2","G3",'failures','studytime','absences']]
-print(data.head())
+#!/usr/bin/env python3.7
